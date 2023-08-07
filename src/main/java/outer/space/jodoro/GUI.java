e1f853e511c0ffabbcc9657c75e0a51884611e2b
Bleart Nuredini <nuredini.bleart@gmail.com>
2023-08-08 00:08:31 +0200

    chore: fix typo in comment

--- src/main/java/outer/space/jodoro/GUI.java
+++ src/main/java/outer/space/jodoro/GUI.java
@@ -68,7 +68,7 @@ public class GUI {
     }
 
     /**
-     * Components have been setup in the order they appear in the GUI so it's easier to
+     * Components have been set up in the order they appear in the GUI so it's easier to
      * visualize how everything will look like.
      */
     public GUI() {
